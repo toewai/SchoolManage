d8ea4b9395a2b214a0b7fd70784e24b8b1c0c01e
toewai <toewai100@gmail.com>
2025-06-12 13:46:40 +0630

    Shield add and permission and role created

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->required()
+                    ->maxLength(255)
+                    ->label('Title'),
+                RichEditor::make('content')
+                    ->required()
+                    ->maxLength(65535)
+                    ->label('Content')
+                    ->columnSpanFull(),
+                Select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->required()
+                    ->label('Author')
+                    ->searchable()
+                    ->preload()
+                    ->placeholder('Select an author'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')
+                    ->sortable()
+                    ->searchable()
+                    ->label('Title'),
+                TextColumn::make('content')
+                    ->limit(50)
+                    ->sortable()
+                    ->searchable()
+                    ->label('Content'),
+                TextColumn::make('user.name')
+                    ->sortable()
+                    ->searchable()
+                    ->label('Author'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
